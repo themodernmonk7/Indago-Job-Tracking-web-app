c4a00c0ae402ffecf87e7cd34a4071f6cd1e8b9d
themodernmonk7 <sauravavishek@gmail.com>
2022-10-05 16:11:16 +0530

    React Router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,35 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Error, Landing, Register, ProtectedRoute } from "./pages"
+import {
+  AddJob,
+  AllJobs,
+  Dashboard,
+  Profile,
+  SharedLayout,
+} from "./pages/dashboard"
 function App() {
   return (
-    <>
-        <h2>hello</h2>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              {" "}
+              <SharedLayout />{" "}
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Dashboard />} />
+          <Route path="all-jobs" element={<AllJobs />} />
+          <Route path="add-job" element={<AddJob />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
+        <Route path="landing" element={<Landing />} />
+        <Route path="register" element={<Register />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
