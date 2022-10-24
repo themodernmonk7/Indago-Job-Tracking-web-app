b39190beec27859badb70343dca231ae42fac01e
themodernmonk7 <sauravavishek@gmail.com>
2022-10-24 19:56:32 +0530

    Create static data

---
+++ src/data.jsx
@@ -0,0 +1,111 @@
+import React from "react"
+import Apple from "./assets/Apple.png"
+import Google from "./assets/Google.png"
+import Amazon from "./assets/Amazon.png"
+import Twitter from "./assets/Twitter.png"
+import Behance from "./assets/Behance.png"
+import Dribbble from "./assets/Dribbble.png"
+import Netflix from "./assets/Netflix.png"
+import Slack from "./assets/Slack.png"
+
+// #845EC2
+// #4FFBDF
+// #00C2A8
+// #F3C5FF
+// #D65DB1
+// #FF6F91
+
+const data = [
+  {
+    id: 1,
+    company: "Dribbble",
+    position: "Sr, Product Designer",
+    status: "Interview",
+    jobType: "Full-time",
+    jobLocation: "New York, USA",
+    image: Dribbble,
+  },
+  {
+    id: 2,
+    company: "Slack",
+    position: "Sr, Product Designer",
+    status: "Declined",
+    jobType: "Part-time",
+    jobLocation: "New York, USA",
+    image: Slack,
+  },
+  {
+    id: 3,
+    company: "Behance",
+    position: "Software  Engineer",
+    status: "Pending",
+    jobType: "Remote",
+    jobLocation: "Berlin, Germany",
+    image: Behance,
+  },
+  {
+    id: 4,
+    company: "Twitter",
+    position: "UI/UX designer",
+    status: "Interview",
+    jobType: "Internship",
+    jobLocation: " California, USA",
+    image: Twitter,
+  },
+  {
+    id: 5,
+    company: "Netflix",
+    position: "Backend Engineer",
+    status: "Declined",
+    jobType: "Full-time",
+    jobLocation: "New Delhi, India",
+    image: Netflix,
+  },
+  {
+    id: 6,
+    company: "Amazon",
+    position: "Front-end Engineer",
+    status: "Pending",
+    jobType: "Remote",
+    jobLocation: "Banglore, India",
+    image: Amazon,
+  },
+  {
+    id: 7,
+    company: "Google",
+    position: "Motion Designer",
+    status: "Interview",
+    jobType: "Internship",
+    jobLocation: "Hydrabad, India",
+    image: Google,
+  },
+  {
+    id: 8,
+    company: "Apple",
+    position: "UX Designer",
+    status: "Declined",
+    jobType: "Part-time",
+    jobLocation: "New York, USA",
+    image: Apple,
+  },
+  {
+    id: 9,
+    company: "Dribbble",
+    position: "Sr, Product Designer",
+    status: "Interview",
+    jobType: "Full-time",
+    jobLocation: "New York, USA",
+    image: Dribbble,
+  },
+  {
+    id: 10,
+    company: "Dribbble",
+    position: "Sr, Product Designer",
+    status: "Interview",
+    jobType: "Full-time",
+    jobLocation: "New York, USA",
+    image: Dribbble,
+  },
+]
+
+export default data
