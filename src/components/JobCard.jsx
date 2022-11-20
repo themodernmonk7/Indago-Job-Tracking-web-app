c4fdc067e720c5995a2d799337c8a06659ec622c
themodernmonk7 <sauravavishek@gmail.com>
2022-11-20 20:00:34 +0530

    Add responsiveness

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,65 +1,39 @@
 import React from "react"
 import data from "../data"
 // import { HiEllipsisVertical, HiTrash } from "react-icons/hi2"
-import { HiDotsVertical, HiTrash } from "react-icons/hi"
+import { HiDotsVertical, HiOutlineTrash } from "react-icons/hi"
+import { AiOutlineEdit } from "react-icons/ai"
 
 const JobCard = () => {
   return (
-    <section className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6 my-10 mx-4 md:mx-8 mb-28 ">
+    <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 gap-10 my-10 mb-28 place-items-center ">
+      {/* <div className=" flex justify-between ">
+        <p>75 Jobs found</p>
+        <p>Sort by latest</p>
+      </div> */}
       {data.map((item) => {
         const { id, company, position, status, jobType, jobLocation, image } =
           item
         return (
           <article
             key={id}
-            className= "  bg-gray-100/80 hover:bg-white px-4 xl:px-8 py-4 xl:py-8 rounded-xl hover:shadow-md space-y-4 transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-105 duration-300 shadow-md md:shadow-none  "
+            className="bg-white px-6 md:px-10 py-8 rounded-xl space-y-6 border max-w-xs lg:max-w-sm "
           >
-            <div className="flex items-start justify-between ">
-              <div className="flex justify-center  space-x-4 items-start">
-                {/* Logo */}
-                  <img
-                    src={image}
-                    alt=""
-                    className=" xl:w-14 xl:h-14 w-10 h-10 object-contain border shadow-inner  object-center rounded-full md:bg-white "
-                  />
-                {/* Company and Position */}
-                <div className=" space-y-2 w-full">
-                  <div className="">
-                    <h4 className="xl:text-xl text-lg font-medium text-gray-700">
-                      {position}
-                    </h4>
-                    <p className=" text-sm xl:text-lg font-medium text-gray-700">
-                      {company}, {jobLocation}
-                    </p>
-
-                  </div>
-                  <div className="flex items-center space-x-10 ">
-                    <p
-                      className={`capitalize tracking-wider  font-medium px-2 text-xs py-1 rounded-full ${
-                        jobType === "Full-time" &&
-                        "bg-pink-400/50 text-pink-500"
-                      } ${
-                        jobType === "Part-time" &&
-                        "bg-blue-500/40 text-blue-500"
-                      } ${
-                        jobType === "Internship" &&
-                        "bg-purple-400/40 text-purple-500"
-                      } ${
-                        jobType === "Remote" &&
-                        "bg-fuchsia-400/40 text-fuchsia-500"
-                      } `}
-                    >
-                      {" "}
-                      {jobType}{" "}
-                    </p>
-                    <span className="text-[10px] text-gray-500 font-medium">
-                      22 Oct 2022
-                    </span>
-                  </div>
-                </div>
-              </div>
-              <p
-                className={`uppercase text-[10px] md:text-xs tracking-widest font-medium  px-2 py-1 rounded-full  ${
+            <div className="flex justify-between items-center">
+              <img
+                src={image}
+                alt=""
+                className="xl:w-14 xl:h-14 w-10 h-10 object-contain border shadow-inner  object-center rounded-full md:bg-white"
+              />
+              <span className="text-gray-400 text-sm font-medium ">
+                {" "}
+                7 July{" "}
+              </span>
+            </div>
+            <div className="flex justify-between items-center h-16 ">
+              <h4 className="text-xl font-semibold w-3/5 "> {position} </h4>
+              <span
+                className={`uppercase text-[10px] tracking-widest font-medium  px-2 py-1 rounded-full flex justify-center items-center  ${
                   status === "Declined" && "bg-red-300/70 text-red-700"
                 } ${
                   status === "Interview" && "bg-green-300/70 text-green-700"
@@ -67,18 +41,39 @@ const JobCard = () => {
                   status === "Pending" && "bg-yellow-300/70 text-yellow-700"
                 } `}
               >
-                {status}
-              </p>
+                {" "}
+                {status}{" "}
+              </span>
             </div>
-
-            {/* Job Description */}
             <div className="">
-              <p className="text-gray-600 hidden md:grid ">
-                I applied in this company because my skill set matched the
-                criteria and I am confident enough to crack the interview.
+              <span
+                className={`capitalize tracking-wider border rounded-full px-2 py-1  font-medium text-xs ${
+                  jobType === "Full-time" && " text-pink-500 border-pink-500 "
+                } ${
+                  jobType === "Part-time" && " text-blue-500 border-blue-500 "
+                } ${
+                  jobType === "Internship" &&
+                  " text-purple-500 border-purple-500  "
+                } ${
+                  jobType === "Remote" &&
+                  " text-fuchsia-500 border-fuchsia-500 "
+                } `}
+              >
+                {" "}
+                {jobType}{" "}
+              </span>
+            </div>
+            <div className=" space-y-2 ">
+              <h4 className="text-md font-medium capitalize">
+                {" "}
+                {company}, {jobLocation}
+              </h4>
+              <p className="text-md text-gray-400">
+                {/* Add fading away text effect */}I applied in this company
+                because my skill.{" "}
+                <button className="text-blue-500 text-sm ">..read more</button>
               </p>
             </div>
-
             <button className=" ml-auto block items-center hover:text-gray-500 transition-all  ">
               <HiDotsVertical className="w-5 h-5 " />
             </button>
