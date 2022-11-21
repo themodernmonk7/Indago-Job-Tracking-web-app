11f642367a9e223fe206e8cf3f6ae0c84ab083f7
themodernmonk7 <sauravavishek@gmail.com>
2022-11-21 11:56:40 +0530

    Add input

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,54 +5,21 @@ import { IoLogOut } from "react-icons/io5"
 const Profile = () => {
   return (
     <>
-      <section className="flex justify-between px-4 md:px-8 py-4 xl:py-6">
-        <div className=" flex items-center space-x-6">
-          <div className="border w-14 h-14 md:w-24 md:h-24  rounded-full relative">
+      <section className="flex px-4 md:px-8 py-4 sticky top-0 bg-white z-10 rounded-2xl shadow-lg ">
+        <div className=" flex items-center justify-between  space-x-6 w-full">
+          <h4 className="md:text-2xl">
+            {" "}
+            Welcome back, <span className="font-medium">Sarah!</span>{" "}
+          </h4>
+          <div className=" w-10 h-10 md:w-12 md:h-12  rounded-full">
             <img
               src={userImage}
               alt=""
               className="object-cover object-center w-full h-full  bg-center rounded-full"
             />
-            <span>
-              {" "}
-              <HiCheckBadge className="w-5 h-5 xl:w-6 xl:h-6 absolute -right-1  xl:right-0 -bottom-1 xl:bottom-1 text-green-600" />{" "}
-            </span>
-          </div>
-          <div className=" w-2/3 lg:space-y-2 text-gray-800">
-            <h3 className="md:text-2xl text-base font-semibold">
-              Samantha William
-            </h3>
-            <p className="md:text-sm text-xs  text-gray-500 xl:font-medium">
-              {/* Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Repellendus, dolores? */}
-              Quality Assurance
-            </p>
-          </div>
-        </div>
-        <div className=" flex justify-start lg:hidden items-center">
-          <IoLogOut className="w-6 h-6 " />
-        </div>
-        <div className=" md:flex flex-col justify-between hidden ">
-          <button className="bg-[#f53465]  text-white px-6 py-1 ml-auto rounded-full text-sm font-medium">
-            Edit Profile
-          </button>
-          <div className="flex space-x-6 text-xs ">
-            <p className="flex flex-col ">
-              <span className="font-semibold">1,098</span>
-              Posts
-            </p>
-            <p className="flex flex-col">
-              <span className="font-semibold">1,098</span>
-              Followers
-            </p>
-            <p className="flex flex-col">
-              <span className="font-semibold">1,098</span>
-              Following
-            </p>
           </div>
         </div>
       </section>
-      <hr />
     </>
   )
 }
--- src/pages/dashboard/Profile.jsx
+++ src/pages/dashboard/Profile.jsx
@@ -1,9 +1,129 @@
 import React from "react"
 import { ProfileFormRow } from "../../components"
+import userImage from "../../assets/user.png"
+
 const Profile = () => {
   return (
     <>
-      <ProfileFormRow />
+      <section className=" my-10 space-y-10 px-8 ">
+        <h4 className="text-3xl "> Account Settings </h4>
+        <div className=" space-x-10 text-lg  ">
+          <button className="bg-white px-8 py-2 rounded-xl active:outline outline-2 outline-primary hover:outline">
+            My details
+          </button>
+          <button className="bg-white px-8 py-2 rounded-xl active:outline outline-2 outline-primary hover:outline">
+            Profile
+          </button>
+          <button className="bg-white px-8 py-2 rounded-xl active:outline outline-2 outline-primary hover:outline">
+            Password
+          </button>
+          <button className="bg-white px-8 py-2 rounded-xl active:outline outline-2 outline-primary hover:outline">
+            API
+          </button>
+        </div>
+        <div className=" space-y-2 ">
+          <h4 className="text-2xl">Profile</h4>
+          <p className=" text-gray-500">
+            Update your phone and personal details here.
+          </p>
+        </div>
+        <div className="flex items-center space-x-10">
+          <img
+            src={userImage}
+            alt=""
+            className="w-24 h-24 object-cover rounded-full border "
+          />
+          <div className=" space-x-6 ">
+            <button className="bg-white px-12 py-2 rounded-xl active:outline outline-2 outline-primary hover:outline border border-gray-300 font-medium">
+              Change
+            </button>
+            <button className="bg-white px-12 py-2 rounded-xl active:outline outline-2 outline-primary hover:outline border border-gray-300 font-medium">
+              Remove
+            </button>
+          </div>
+        </div>
+
+        {/* Form */}
+        <form className=" w-2/3 space-y-6 ">
+          <div className="flex items-center justify-between space-x-28 ">
+            {/* First name */}
+            <div className=" flex flex-col space-y-2 w-full ">
+              <label
+                htmlFor=""
+                className="text-gray-500 after:content-['*'] after:ml-1 after:text-red-500  "
+              >
+                First name
+              </label>
+              <input
+                type="text"
+                placeholder="your first name"
+                className=" py-3  rounded-xl border-none ring-1 ring-gray-300 focus:ring-2 focus:ring-primary placeholder:text-gray-300 placeholder:text-sm shadow "
+              />
+              {/* rounded-xl border-2 border-gray-200 placeholder:text-sm focus:outline-none focus:ring-primary focus:ring-1 hover:ring-primary hover:ring-1 py-4 placeholder:text-gray-300 transition-all duration-200 ease-in-out delay-100 text-lg */}
+            </div>
+            {/* Last name */}
+            <div className="flex flex-col space-y-2 w-full ">
+              <label htmlFor="" className="text-gray-500">
+                Last name
+              </label>
+              <input
+                type="text"
+                placeholder=" your last name"
+                className="py-3  rounded-xl border-none ring-1 ring-gray-300 focus:ring-2 focus:ring-primary placeholder:text-gray-300 placeholder:text-sm shadow"
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center justify-between space-x-28">
+            {/* Email */}
+            <div className="flex flex-col space-y-2 w-full ">
+              <label
+                htmlFor=""
+                className="text-gray-500 after:content-['*'] after:ml-1 after:text-red-500"
+              >
+                Email
+              </label>
+              <input
+                type="email"
+                placeholder="youremail@gmai.com"
+                className=" py-3  rounded-xl border-none ring-1 ring-gray-300 focus:ring-2 focus:ring-primary placeholder:text-gray-300 placeholder:text-sm shadow"
+              />
+            </div>
+            {/* Location */}
+            <div className="flex flex-col space-y-2 w-full ">
+              <label htmlFor=" " className="text-gray-500">
+                Location
+              </label>
+              <input
+                type="text"
+                placeholder="my city"
+                className=" py-3  rounded-xl border-none ring-1 ring-gray-300 focus:ring-2 focus:ring-primary placeholder:text-gray-300 placeholder:text-sm shadow"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col space-y-2 ">
+            <label htmlFor="" className="text-gray-500 ">
+              Bio
+            </label>
+            <textarea
+              name=""
+              id=""
+              cols="30"
+              rows="5"
+              placeholder="Brief description for your profile..."
+              className=" py-3  rounded-xl border-none ring-1 ring-gray-300 focus:ring-2 focus:ring-primary placeholder:text-gray-300 placeholder:text-sm resize-none shadow "
+            ></textarea>
+          </div>
+          <div className=" space-x-6 flex justify-end  ">
+            <button className=" px-10 py-2 rounded-xl active:outline outline-2 outline-primary hover:outline border border-gray-300 font-medium bg-primary text-white ">
+              Save
+            </button>
+            <button className="bg-white px-10 py-2 rounded-xl active:outline outline-2 outline-primary hover:outline border border-gray-300 font-medium">
+              Cancel
+            </button>
+          </div>
+        </form>
+      </section>
     </>
   )
 }
