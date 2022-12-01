794e5b9bb36e39617de93fbdc76871cfbf4c5b1d
themodernmonk7 <sauravavishek@gmail.com>
2022-12-01 21:04:27 +0530

    Form setup

--- src/components/ProfileForm.jsx
+++ src/components/ProfileForm.jsx
@@ -1,29 +1,83 @@
+import { useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { toast } from "react-toastify"
 import { FormRow, Button } from "../components"
+import { updateUser } from "../features/user/userSlice"
 
 const ProfileForm = () => {
+  const { user, isLoading } = useSelector((store) => store.user)
+  const dispatch = useDispatch()
+  const [userData, setUserData] = useState({
+    name: user?.name || "",
+    lastName: user?.lastName || "",
+    email: user?.email || "",
+    location: user?.location || "",
+    bio: user?.bio || "",
+  })
+
+  const handleChange = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+    setUserData({ ...userData, [name]: value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const { name, lastName, email, location, bio } = userData
+    if (!name || !lastName || !email || !location || !bio) {
+      toast.error("Please fill out all fields")
+      return
+    }
+    dispatch(updateUser({ name, lastName, email, location, bio }))
+  }
+
   return (
-    <form className=" xl:w-2/3 space-y-6 ">
+    <form className=" xl:w-2/3 space-y-6 " onSubmit={handleSubmit}>
       <div className="grid md:grid-cols-2 md:gap-x-28 gap-y-8">
         {/* First Name */}
-        <FormRow type="text" name=" First Name" placeholder="Sarah" />
+        <FormRow
+          type="text"
+          labelText="First Name"
+          name="name"
+          placeholder="Sarah"
+          value={userData.name}
+          handleChange={handleChange}
+        />
         {/* Last name */}
-        <FormRow type="text" name=" Last Name" placeholder="Williams" />
+        <FormRow
+          type="text"
+          labelText="Last Name"
+          name="lastName"
+          placeholder="Williams"
+          handleChange={handleChange}
+          value={userData.lastName}
+        />
         {/* Email */}
-        <FormRow type="email" name=" Email" placeholder="youremail@gmail.com" />
+        <FormRow
+          type="email"
+          name="email"
+          placeholder="youremail@gmail.com"
+          handleChange={handleChange}
+          value={userData.email}
+        />
         {/* Location */}
         <FormRow
           type="text"
-          name="Location"
+          name="location"
           placeholder="My city"
           label_style_className="after:content-none"
+          handleChange={handleChange}
+          value={userData.location}
         />
         {/* Bio */}
         <div className="md:col-span-2">
           <FormRow
             type="text"
-            name="Bio"
+            name="bio"
             textArea={true}
             placeholder="Brief description about yourself..."
+            handleChange={handleChange}
+            value={userData.bio}
           />
         </div>
       </div>
