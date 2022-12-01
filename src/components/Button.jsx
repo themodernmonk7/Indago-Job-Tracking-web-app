5bec50cce3899540d5509ac73b99cd7c0affcbf4
themodernmonk7 <sauravavishek@gmail.com>
2022-12-01 21:43:24 +0530

    Fix: button type

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,10 @@
 const Button = ({ props }) => {
   return (
     <div className=" space-x-6 flex md:justify-end  ">
-      <button className=" px-10 py-2 rounded-xl active:outline outline-2 outline-primary hover:outline border border-gray-300 font-medium bg-primary text-white capitalize ">
+      <button
+        type="submit"
+        className=" px-10 py-2 rounded-xl active:outline outline-2 outline-primary hover:outline border border-gray-300 font-medium bg-primary text-white capitalize "
+      >
         {props[0]}
       </button>
       <button className="bg-white px-10 py-2 rounded-xl active:outline outline-2 outline-primary hover:outline border border-gray-300 font-medium capitalize ">
