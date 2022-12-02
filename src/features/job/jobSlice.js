96bf63b334e571acea2126ebffd6489aba5504bf
themodernmonk7 <sauravavishek@gmail.com>
2022-12-02 09:32:19 +0530

    Create Job slice

---
+++ src/features/job/jobSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {}
+
+export const jobSlice = createSlice({
+  name: "Job",
+  initialState,
+  reducers: {},
+  extraReducers: {},
+})
+
+export const {} = jobSlice.actions
+export default jobSlice.reducer
