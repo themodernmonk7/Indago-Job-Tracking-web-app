59b737f612d7f6b31925e48dee8795843385404c
themodernmonk7 <sauravavishek@gmail.com>
2022-12-02 15:49:00 +0530

    Add missing props

--- src/components/FormRowSelect.jsx
+++ src/components/FormRowSelect.jsx
@@ -1,13 +1,24 @@
-const FormRowSelect = ({ labelText, name, options }) => {
+const FormRowSelect = ({ labelText, name, options, value, handleChange }) => {
   return (
     <div className="input_container ">
       <label htmlFor={name} className="label_style">
         {labelText || name}
       </label>
-      <select name={name} id={name} className="input_style capitalize ">
-        <option value="full-time">full-time</option>
-        <option value="part-time">part-time</option>
-        <option value="internship">internship</option>
+      <select
+        name={name}
+        id={name}
+        value={value}
+        onChange={handleChange}
+        className="input_style capitalize "
+      >
+        {options.map((value, index) => {
+          return (
+            <option key={index} value={value}>
+              {" "}
+              {value}{" "}
+            </option>
+          )
+        })}
       </select>
     </div>
   )
