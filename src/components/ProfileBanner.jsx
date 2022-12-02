d65158482008db60f3ee6012ff1e093339a795af
themodernmonk7 <sauravavishek@gmail.com>
2022-12-02 09:24:53 +0530

    Add user name on banner

--- src/components/ProfileBanner.jsx
+++ src/components/ProfileBanner.jsx
@@ -1,12 +1,16 @@
+import { useSelector } from "react-redux"
 import UserImage from "./UserImage"
+
 const ProfileBanner = () => {
+  const { user } = useSelector((store) => store.user)
   return (
     <>
       <section className="flex px-4 md:px-8 py-4 sticky top-0 bg-white z-10 lg:rounded-2xl lg:my-10 border-b backdrop-blur-sm shadow-md ">
         <div className=" flex items-center justify-between  space-x-6 w-full">
           <h4 className="md:text-2xl">
             {" "}
-            Welcome back, <span className="font-medium">Sarah!</span>{" "}
+            Welcome back,{" "}
+            <span className="font-medium"> {`${user.name}!`}</span>{" "}
           </h4>
           <UserImage className={"w-10 h-10 md:w-14 md:h-14"} />
         </div>
