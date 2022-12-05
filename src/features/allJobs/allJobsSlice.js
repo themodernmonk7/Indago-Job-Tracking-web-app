328fef86609768296b2b29ec25885f35f4e67e06
themodernmonk7 <sauravavishek@gmail.com>
2022-12-05 18:18:39 +0530

    Get stats and Monthly Applications

--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -8,6 +8,8 @@ const initialState = {
   total_jobs: 0,
   num_of_pages: 1,
   page: 1,
+  stats: [],
+  monthlyApplications: [],
 }
 
 export const getAllJobs = createAsyncThunk(
@@ -19,6 +21,23 @@ export const getAllJobs = createAsyncThunk(
           authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
         },
       })
+      return response.data
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data.msg)
+    }
+  }
+)
+
+export const showStats = createAsyncThunk(
+  "allJobs/showStats",
+  async (_, thunkAPI) => {
+    try {
+      const response = await customFetch.get("/jobs/stats", {
+        headers: {
+          authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
+        },
+      })
+      console.log(response.data)
       return response.data
     } catch (error) {
       return thunkAPI.rejectWithValue(error.response.data.msg)
@@ -43,6 +62,19 @@ export const allJobsSlice = createSlice({
       state.num_of_pages = numOfPages
     },
     [getAllJobs.rejected]: (state, action) => {
+      state.isLoading = false
+      toast.error(action.payload)
+    },
+    [showStats.pending]: (state) => {
+      state.isLoading = true
+    },
+    [showStats.fulfilled]: (state, action) => {
+      const { defaultStats, monthlyApplications } = action.payload
+      state.isLoading = false
+      state.stats = defaultStats
+      state.monthlyApplications = monthlyApplications
+    },
+    [showStats.rejected]: (state, action) => {
       state.isLoading = false
       toast.error(action.payload)
     },
