c8880d96b828396825b0717ac2800e1ea262ba17
themodernmonk7 <sauravavishek@gmail.com>
2022-12-06 21:19:12 +0530

    Removed: code

--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -37,7 +37,6 @@ export const showStats = createAsyncThunk(
           authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
         },
       })
-      console.log(response.data)
       return response.data
     } catch (error) {
       return thunkAPI.rejectWithValue(error.response.data.msg)
