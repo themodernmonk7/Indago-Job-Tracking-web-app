eb14c2e1e5b6fa149b262ff34ef56ff7194a1531
themodernmonk7 <sauravavishek@gmail.com>
2023-01-01 17:33:11 +0530

    Add page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Navbar } from "../components"
+import { Navbar, Logo } from "../components"
 import PhoneMockup from "../assets/rightimg.png"
 import { Link } from "react-router-dom"
 
@@ -11,7 +11,8 @@ const Landing = () => {
         {/* Right */}
         <div className=" flex flex-col justify-between ">
           <div className=" space-y-8 ">
-            <h1 className="text-6xl font-medium max-w-[24.2rem] ">
+            {/* max-w-[24.2rem] */}
+            <h1 className="text-6xl font-[600] max-w-[24.2rem]  ">
               Make the best move to choose your new job
             </h1>
             <p className=" w-3/5 text-lg ">
@@ -32,6 +33,20 @@ const Landing = () => {
           <img src={PhoneMockup} alt="" className=" w-96 h-96 object-cover " />
         </div>
       </section>
+      {/* Bottom  */}
+      <section className=" container mx-auto ">
+        <div className="">
+          {/* Logo */}
+          <Logo className=" h-10" />
+          <h3>Take your chance to work in a place that matters to you</h3>
+        </div>
+        <div className="">
+          <button className="">Register</button>
+          <button className="">Sing In Now</button>
+        </div>
+      </section>
+      {/* Footer */}
+      <footer className="">footer</footer>
     </>
   )
 } 
