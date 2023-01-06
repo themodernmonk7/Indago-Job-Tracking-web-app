60750204d81a59ae541326fac40b1304a5564916
themodernmonk7 <sauravavishek@gmail.com>
2023-01-06 15:24:02 +0530

    Fix Logo

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -12,7 +12,8 @@ const Sidebar = () => {
           <div>
             {/* Logo title */}
             <div className=" flex justify-center items-center space-x-2 mt-7 ">
-              <Logo className="h-10" />
+              <Logo className="h-8" />
+              <h3 className="text-3xl font-medium">Indago</h3>
             </div>
             {/* Menu items */}
             <div className=" mt-28 capitalize flex flex-col justify-center items-center space-y-10 ">
