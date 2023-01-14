26d2db630da215fa15abc96da1b8f156055f078a
themodernmonk7 <sauravavishek@gmail.com>
2023-01-14 20:59:07 +0530

    Refactor design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,20 +3,26 @@ import { Logo } from "../components"
 
 const Navbar = () => {
   return (
-    <nav className="container mx-auto flex justify-between items-center py-4 ">
-      <div className=" py-4 flex items-center space-x-2 ">
-        <Logo className="h-10" />
-        <h2 className="text-3xl text-center font-semibold ">indago</h2>
+    <nav className="container mx-auto flex justify-between items-center my-4 py-2 px-4 ">
+      <div className=" flex items-center space-x-2 ">
+        <Logo className=" h-6 md:h-8 " />
+        <h2 className=" text-xl md:text-3xl text-center font-semibold ">
+          indago
+        </h2>
       </div>
-      <div className=" space-x-12 text-lg ">
-        <Link>Find Jobs</Link>
-        <Link>Sign In</Link>
+      <div className=" md:space-x-8  ">
         <Link
           to="/register"
-          className="bg-gray-100 px-4 py-2 rounded-md text-primary font-medium "
+          className=" font-medium rounded-md hover:bg-gray-100 px-4 py-3 "
+        >
+          Log in
+        </Link>
+        <Link
+          to="/register"
+          className=" bg-secondary-700 px-4 py-3 rounded-md text-white text-xs md:text-sm font-medium hover:bg-secondary-800 "
         >
           {" "}
-          Register{" "}
+          Get Started{" "}
         </Link>
       </div>
     </nav>
