c85cb0ac523becafa0df8b793574b38a9c3e5af9
themodernmonk7 <sauravavishek@gmail.com>
2023-01-18 12:21:01 +0530

    Add default user avatar

--- src/components/UserImage.jsx
+++ src/components/UserImage.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { useSelector } from "react-redux"
-import defaultUserImage from "../assets/user.jpg"
+import defaultUserImage from "../assets/default_user_avatar.jpg"
 const UserImage = ({ className }) => {
   const { user } = useSelector((store) => store.user)
   return (
     <img
-      src={user.image}
+      src={user?.image || defaultUserImage}
       className={`${className} object-top object-cover rounded-full `}
       alt={"profile picture"}
     />
