73548ec092317df611897e8591be1d1e1b0007ca
themodernmonk7 <sauravavishek@gmail.com>
2023-01-19 21:31:15 +0530

    Change graph color

--- src/components/AreaChart.jsx
+++ src/components/AreaChart.jsx
@@ -10,13 +10,13 @@ import {
 } from "recharts"
 const AreaChartComponent = ({ data }) => {
   return (
-    <ResponsiveContainer width="90%" height={300}>
+    <ResponsiveContainer width="90%" height={300} className="mr-12 w-12 ">
       <AreaChart data={data} margin={{ top: 50 }}>
         <CartesianGrid strokeDasharray="1 1" />
         <XAxis dataKey="date" />
         <YAxis allowDecimals={false} />
         <Tooltip />
-        <Area type="monotone" dataKey="count" stroke="#1e3a8a" fill="#3b82f6" />
+        <Area type="monotone" dataKey="count" stroke="#1e3a8a" fill="#1d4ed8" />
       </AreaChart>
     </ResponsiveContainer>
   )
--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -10,13 +10,14 @@ import {
 } from "recharts"
 const BarChartComponent = ({ data }) => {
   return (
-    <ResponsiveContainer width="90%" height={300}>
+    // <ResponsiveContainer width="100%" height={200} className="mr-12">
+    <ResponsiveContainer width="90%" height={300} className="mr-12 w-12 ">
       <BarChart data={data} margin={{ top: 10 }}>
         <CartesianGrid strokeDasharray="1 1" />
         <XAxis dataKey="date" />
         <YAxis allowDecimals={false} />
         <Tooltip />
-        <Bar dataKey="count" fill="#3b82f6" barSize={50} />
+        <Bar dataKey="count" fill="#1d4ed8" barSize={50} />
       </BarChart>
     </ResponsiveContainer>
   )
