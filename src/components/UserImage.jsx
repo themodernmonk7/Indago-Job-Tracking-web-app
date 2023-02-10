0fb25aafff2f3d783cb6811ca7aeb5ff2365634b
themodernmonk7 <sauravavishek@gmail.com>
2023-02-10 16:46:35 +0530

    Add default user image

--- src/components/UserImage.jsx
+++ src/components/UserImage.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import { useSelector } from "react-redux"
-import defaultUserImage from "../assets/default_user_avatar.jpg"
 const UserImage = ({ className }) => {
   const { user } = useSelector((store) => store.user)
   return (
     <img
-      src={user?.image || defaultUserImage}
+      src={user.image}
       className={`${className} rounded-full object-cover object-top `}
       alt={"profile picture"}
     />
